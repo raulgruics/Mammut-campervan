74e8d415e53fc7760ca11f6804ba6babb45f3d2e
raulgruics <94356500+raulgruics@users.noreply.github.com>
2022-02-16 08:04:49 -1200

    fixing-fonts

--- About.js
+++
@@ -1,6 +0,0 @@
-import { gsap } from "gsap";
-import { EaselPlugin } from "gsap/EaselPlugin";
-
-gsap.registerPlugin(EaselPlugin);
-
-gsap.from('.text-block' , {duration: 1, y: '-100%', ease:'bounce' })
